11095d27a5c20fa6af5391c914eedd25175f3c72
LAPTOP-U77VULRG\sun <sun.xy.piao@gmail.com>
2022-10-20 17:12:42 +0800

    temp

--- boot-01-helloworld/src/main/java/com/sunxy/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/sunxy/boot/MainApplication.java
@@ -2,6 +2,7 @@ package com.sunxy.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 主程序类
@@ -11,6 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
     public static void main(String[] args){
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(MainApplication.class, args)
     }
 }
